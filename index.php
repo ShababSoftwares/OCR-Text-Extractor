4242776537a6d977e4757858069b4c98b4972765
Arif Shabab <ml.arifshabab@gmail.com>
2024-01-18 21:27:48 +0500

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<html>
+<body>
+
+<center>
+<h3>PHP OCR Extractor</h3>
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+<input type="file" name="image" />
+<input type="submit"/>
+</form>
+</center>
+
+</body>
+</html>
